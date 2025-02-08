9c4844cb5dcd327bd8df63147c5b0578dc84c1df
Binny Tewani <btewani@newrelic.com>
2025-02-08 16:08:31 +0530

    testing

---
+++ src/main/java/org/example/controller/MyController.java
@@ -0,0 +1,53 @@
+package org.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MyController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    @GetMapping("/serviceA/api/s1")
+    public String service1() {
+        String s2Response = restTemplate.getForObject("http://localhost:8081/serviceB/api/s1", String.class);
+        return "Response from ServiceA - api1 and " + s2Response;
+    }
+
+    @GetMapping("/serviceA/api/s2")
+    public String service2() {
+        String s2Response = restTemplate.getForObject("https://saiteja1.free.beeceptor.com/hello", String.class);
+        return "Response from ServiceA - api2 and " + s2Response;
+    }
+
+    @GetMapping("/serviceA/api/s3")
+    public String service3() {
+        String s2Response = restTemplate.getForObject("http://localhost:8081/serviceB/api/s3", String.class);
+        return "Response from ServiceA - api3 and " + s2Response;
+    }
+
+    @GetMapping("/serviceA/api/s4")
+    public String service4() {
+       try{
+           String s2Response = restTemplate.getForObject("http://localhost:8081/serviceB/api/s4", String.class);
+           return "Response from ServiceA - api4 and " + s2Response;
+       }catch (Exception e){
+           throw new ArrayIndexOutOfBoundsException();
+       }
+
+
+    }
+
+    @GetMapping("/serviceA/api/s5")
+    public String service5() {
+        try{
+            String s2Response = restTemplate.getForObject("http://localhost:8081/serviceB/api/s5", String.class);
+            return "Response from ServiceA - api5 and " + s2Response;
+        }catch (Exception e){
+            return  "Dont worry";
+        }
+    }
+}
