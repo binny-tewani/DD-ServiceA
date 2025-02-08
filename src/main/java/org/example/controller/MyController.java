88a8c7cc4a886ff739d5c95cd34bced93b9221c6
Binny Tewani <btewani@newrelic.com>
2025-02-08 17:48:34 +0530

    added new api with partial error

--- src/main/java/org/example/controller/MyController.java
+++ src/main/java/org/example/controller/MyController.java
@@ -1,14 +1,19 @@
 package org.example.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
 import java.util.Random;
 
 @RestController
 public class MyController {
+    private static final Log log = LogFactory.getLog(MyController.class);
+
     @Autowired
     private RestTemplate restTemplate;
     private final Random random = new Random();
@@ -16,7 +21,7 @@ public class MyController {
     @GetMapping("/serviceA/api/s1")
     public String service1() {
         if (random.nextInt(4) == 0) {
-            throw new NullPointerException("Simulated NullPointerException for testing");
+            throw new NullPointerException("OPOPOPOP- NullPointerException for testing");
         }
         String s2Response = restTemplate.getForObject("http://localhost:8081/serviceB/api/s1", String.class);
         return "Response from ServiceA - api1 and " + s2Response;
@@ -55,4 +60,19 @@ public class MyController {
             return  "Dont worry";
         }
     }
+
+    @GetMapping("/serviceA/api/s6")
+    public Map<String, Object> service6() {
+        try{
+            Map<String, Object> res = restTemplate.getForObject("http://localhost:8081/serviceB/api/s6", Map.class);
+            Integer resCode = (Integer)res.get("status");
+            System.out.println("Response code is " + resCode);
+            log.info("Response code is " + resCode);
+           return res;
+        }catch (Exception e){
+            throw new RuntimeException("Dont worry");
+        }
+    }
+
+
 }
